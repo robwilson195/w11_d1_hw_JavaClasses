9407d777e3a8eb931a396d78265562ae5fd3595a
Rob Wilson <rob.wilson195@gmail.com>
2019-01-07 16:27:05 +0000

    Started water bottle tests. Bottles can have volume.

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,13 @@
 public class WaterBottle {
+
+    int volume;
+
+
+    public WaterBottle() {
+        this.volume = 100;
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    WaterBottle bottle;
+    @Before
+    public void before() {
+        bottle = new WaterBottle();
+    }
+
+    @Test
+    public void hasVolume() {
+        assertEquals(100, bottle.getVolume());
+    }
+
 }
