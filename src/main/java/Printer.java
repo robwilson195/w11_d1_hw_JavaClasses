8d4caf050785d67fd6a5a58c992f886ade5eae03
Rob Wilson <rob.wilson195@gmail.com>
2019-01-07 17:01:44 +0000

    Finished the printer to the extent required by the extensions.

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,11 +1,29 @@
 public class Printer {
     private int paper;
+    private int toner;
 
-    public Printer(int paper) {
+    public Printer(int paper, int toner) {
         this.paper = paper;
+        this.toner = toner;
     }
 
     public int getPaper() {
         return this.paper;
+    }
+
+    public void print(int pages, int copies) {
+        int reqPaper = pages * copies;
+        if (reqPaper <= this.paper) {
+            this.paper -= reqPaper;
+            this.toner -= reqPaper;
+        }
+    }
+
+    public void refillPaper() {
+        this.paper = 20;
+    }
+
+    public int getToner() {
+        return toner;
     }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -8,12 +8,43 @@ public class PrinterTest {
 
     @Before
     public void before() {
-        printer = new Printer(20);
+        printer = new Printer(20, 50);
     }
 
     @Test
     public void shouldHavePaperStock() {
         int expected = 20;
         assertEquals(expected, printer.getPaper());
+    }
+
+    @Test
+    public void shouldFailToPrintIfInsufficientPaper() {
+        printer.print(4, 6);
+        assertEquals(20, printer.getPaper());
+    }
+
+    @Test
+    public void shouldPrintIfSufficientPaper() {
+        printer.print(3, 6);
+        assertEquals(2, printer.getPaper());
+    }
+
+    @Test
+    public void shouldBeAbleToRefill() {
+        printer.print(3, 6);
+        assertEquals(2, printer.getPaper());
+        printer.refillPaper();
+        assertEquals(20, printer.getPaper());
+    }
+
+    @Test
+    public void shouldHaveToner() {
+        assertEquals(50, printer.getToner());
+    }
+
+    @Test
+    public void shouldLoseTonerUponPrinting() {
+        printer.print(3, 6);
+        assertEquals(32, printer.getToner());
     }
 }
